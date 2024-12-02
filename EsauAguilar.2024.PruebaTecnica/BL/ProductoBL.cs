0dc424c42c614b69c218af49ad838b33390f421d
Albertito21 <815248@clases.edu.sv>
2024-12-02 16:23:36 -0600

    Agregar archivos de proyecto.

---
+++ EsauAguilar.2024.PruebaTecnica/BL/ProductoBL.cs
@@ -0,0 +1,18 @@
+﻿using EsauAguilar._2024.PruebaTecnica.DAL;
+using EsauAguilar._2024.PruebaTecnica.Models;
+
+namespace EsauAguilar._2024.PruebaTecnica.BL
+{
+    public class ProductoBL 
+    { 
+        private ProductoDAL productoDAL = new ProductoDAL();
+        public List<Producto> ObtenerTodosLosProductos() 
+        { 
+            return productoDAL.ObtenerProductos(); 
+        } 
+        public void InsertarProducto(Producto producto) 
+        { 
+            productoDAL.InsertarProducto(producto); 
+        } 
+    }
+}
